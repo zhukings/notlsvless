f330363fe5b2369c02f515fd25c38702d2900539
zhukings <143058184+zhukings@users.noreply.github.com>
2024-03-30 11:08:23 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -12,17 +12,24 @@ let addresses = [
 // 设置优选地址api接口
 let addressesapi = [
 	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt', //可参考内容格式 自行搭建。
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
-];
-
-// 设置优选地址，不带端口号默认80，noTLS订阅生成
-let addressesapi = [
-  	'https://raw.githubusercontent.com/zhukings/cf-rule/main/yxip.txt',
+//	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesipv6api.txt', //IPv6优选内容格式 自行搭建。
+	'https://raw.githubusercontent.com/zhukings/cf-rule/main/yxip.txt',
 //	'https://raw.githubusercontent.com/zhukings/vless2sub/main/addressesapi.txt',
-  	'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressesapi.txt',
 //	'https://addressesapi.090227.xyz/CloudFlareYes',
   	'https://addressesapi.090227.xyz/ct',
 //	'https://addressesapi.090227.xyz/cmcc', 自行搭建。
+];
+
+// 设置优选地址，不带端口号默认80，noTLS订阅生成
+let addressesnotls = [
+	'www.visa.com.sg#官方优选域名',
+	'www.wto.org:8080#官方优选域名',
+	'www.who.int:8880#官方优选域名',
+];
+
+// 设置优选noTLS地址api接口
+let addressesnotlsapi = [
+	'https://raw.githubusercontent.com/cmliu/CFcdnVmess2sub/main/addressesapi.txt',
 ];
 
 let DLS = 10;//速度下限
